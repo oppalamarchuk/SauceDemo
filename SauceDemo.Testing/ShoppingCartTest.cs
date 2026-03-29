51c4ee370601d032b1ac820f7945d1e061783701
Ostap <palamarcukostap7@gmail.com>
2026-03-29 13:28:26 +0300

    Initial commit

---
+++ SauceDemo.Testing/ShoppingCartTest.cs
@@ -0,0 +1,15 @@
+namespace SauceDemo.Testing;
+
+public class ShoppingCartTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
